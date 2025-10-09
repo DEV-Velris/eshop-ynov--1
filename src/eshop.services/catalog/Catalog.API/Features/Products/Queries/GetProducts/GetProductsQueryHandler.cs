2de96da744dcf3309d30ec6338df939f88623e58
Axel Augis <augisax@gmail.com>
2025-10-09 11:18:21 +0200

    Added multiple filters for queries

--- src/eshop.services/catalog/Catalog.API/Features/Products/Queries/GetProducts/GetProductsQueryHandler.cs
+++ src/eshop.services/catalog/Catalog.API/Features/Products/Queries/GetProducts/GetProductsQueryHandler.cs
@@ -19,7 +19,6 @@ public class GetProductsQueryHandler(IDocumentSession documentSession) : IQueryH
     public async Task<GetProductsQueryResult> Handle(GetProductsQuery request, CancellationToken cancellationToken)
     {
 
-        // Create a queryable collection of products from the document session
         var query = documentSession.Query<Product>().AsQueryable();
 
         if (!string.IsNullOrEmpty(request.Category))
