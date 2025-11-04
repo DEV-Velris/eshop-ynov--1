06b0fc0a0e59fce0d1684b2adcf2c2bf05e4ef0a
Alexis <velris.dev@gmail.com>
2025-11-04 10:13:55 +0100

    Ajout Swagger

--- src/eshop.services/discount/Discount.Grpc/Program.cs
+++ src/eshop.services/discount/Discount.Grpc/Program.cs
@@ -9,6 +9,7 @@ var configuration = builder.Configuration;
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddGrpc().AddJsonTranscoding();
 
@@ -26,5 +27,11 @@ app.MapGrpcService<DiscountServiceServer>();
 app.MapGet("/",
     () =>
         "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.Run();
